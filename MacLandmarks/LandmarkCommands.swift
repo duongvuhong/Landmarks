1ffe44f97f7d50025c348213291207c8fe2bce8b
Vu Hong Duong <duongvh.dev@gmail.com>
2023-10-28 18:17:31 +0900

    feat: Add a Built-in Menu Command

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by Vu Hong Duong on 2023/10/28.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
